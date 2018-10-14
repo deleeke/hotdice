581d0528eb1648c188e46429f9773d751b940c31
Bianca <beeankha@gmail.com>
2018-10-14 16:13:00 -0400

    My stuff

--- games/games/urls.py
+++ games/games/urls.py
@@ -1,4 +1,4 @@
-"""hotdice URL Configuration
+"""games URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path('hotdice/', include('hotdice.urls')),
+    path('admin/', admin.site.urls),
+]
