8f1eb215dd36d68f7ecd21f64f3be8a3ae25442d
Bianca <beeankha@gmail.com>
2018-10-27 15:40:50 -0400

    Did the things for part 2 of Django tutorial

--- games/hotdice/models.py
+++ games/hotdice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import random
 
-# Create your models here.
+
+class Game(models.Model):
+    num_players = models.IntegerField(default=1)
+    num_dice = models.IntegerField(default=6)
+    max_pips = models.IntegerField(default=6)
+
+
+class Player(models.Model):
+    player_name = models.CharField(max_length=50)
+    score = models.IntegerField(default=0)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
